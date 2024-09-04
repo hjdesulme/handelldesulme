5a870b0c259103563bba9033fab87ae89ca36ca4
Handell Desulme <tue73811@temple.edu>
2024-09-03 21:23:05 -0400

    updates build.sh to use extended version of Hugo

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 # Step 1: Specify Hugo Version
 HUGO_VERSION="0.123.8"
 
-# Step 2: Fetch Hugo
-echo "Fetching Hugo $HUGO_VERSION..."
-curl -L https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz | tar xz
+# Step 2: Fetch Hugo Extended
+echo "Fetching Hugo Extended $HUGO_VERSION..."
+curl -L https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz | tar xz
 mv hugo /usr/local/bin/
 
 # Step 3: Build the Project
