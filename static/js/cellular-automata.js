62e1578c32ecbba181ee4541655c3c729d473130
Handell Desulme <tue73811@temple.edu>
2024-09-03 22:57:12 -0400

    adds my images to Game of Life cells, favicon, & 'About Me'

--- static/js/cellular-automata.js
+++ static/js/cellular-automata.js
@@ -5,11 +5,25 @@ class CellularAutomata {
     this.resizeCanvas();
     this.initGrid();
 
-    this.render();
-    setInterval(() => this.update(), 100);
+    this.cellImage = new Image();
+    this.cellImage.src = '/images/forCells.jpg';
+    this.cellImage.onload = () => {
+      console.log('Cell image loaded successfully');
+      this.render();
+    };
+    this.cellImage.onerror = () => {
+      console.error('Failed to load cell image');
+    };
 
     this.canvas.addEventListener('click', (e) => this.handleClick(e));
     window.addEventListener('resize', () => this.handleResize());
+
+    // Start updating only after the image has loaded
+    this.cellImage.onload = () => {
+      console.log('Cell image loaded successfully');
+      this.render();
+      setInterval(() => this.update(), 100);
+    };
   }
 
   resizeCanvas() {
@@ -54,14 +68,28 @@ class CellularAutomata {
 
   render() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    if (!this.cellImage || !this.cellImage.complete) {
+      console.log('Cell image not yet loaded');
+      return;
+    }
     this.grid.forEach((row, x) => {
       row.forEach((cell, y) => {
         if (cell) {
-          this.ctx.fillStyle = 'rgba(59, 130, 246, 0.5)';
-          this.ctx.fillRect(y * this.cellSize, x * this.cellSize, this.cellSize, this.cellSize);
+          if (this.isPartOfBlock(x, y)) {
+            this.ctx.drawImage(this.cellImage, y * this.cellSize * 2, x * this.cellSize * 2, this.cellSize * 2, this.cellSize * 2);
+          } else {
+            this.ctx.drawImage(this.cellImage, y * this.cellSize, x * this.cellSize, this.cellSize, this.cellSize);
+          }
         }
       });
     });
+  }
+
+  isPartOfBlock(x, y) {
+    if (x < this.rows - 1 && y < this.cols - 1) {
+      return this.grid[x][y] && this.grid[x+1][y] && this.grid[x][y+1] && this.grid[x+1][y+1];
+    }
+    return false;
   }
 
   handleClick(e) {
